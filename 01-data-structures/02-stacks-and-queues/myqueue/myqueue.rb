483bf460d07c7ba7538a00e977ecb4bc9897a81a
Tara Willman <tara.willman@gmail.com>
2017-03-21 00:29:44 -0700

    stacks and queues

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,34 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    self.tail = @queue[0]
+    @h=-1
+    @t=0
   end
 
   def enqueue(element)
+    @h += 1
+    @queue[@h] = element
+    @head = @queue[@h]
+    self.tail = @queue[@t]
   end
 
   def dequeue
+    temp = @queue[@t]
+    if (!empty?)
+      @t += 1
+      self.tail = @queue[@t]
+    else
+      self.tail = nil
+    end
+    return temp
   end
 
   def empty?
+    if (@h - @t >= 0)
+      return false
+    end
+    @t=0
+    return true
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -4,14 +4,31 @@ class MyStack
   def initialize
     @stack = Array.new
     self.top = nil
+    @p = -1
   end
 
   def push(item)
+    @p += 1
+    @stack[@p] = item
+    self.top =@stack[@p]
   end
 
   def pop
+    temp = top
+    @p -= 1
+    if (!empty?)
+      self.top = @stack[@p]
+    else
+      self.top = nil
+    end
+    return temp
   end
 
   def empty?
+    if (@p>=0)
+      return false
+    end
+    @p = -1
+    return true
   end
 end
